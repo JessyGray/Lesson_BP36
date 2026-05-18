09b7afbd10a63f2987a73be34d853d48298a352c
Jessy Gray <Jessy_Gray@icloud.com>
2026-05-18 12:44:44 -0700

    ghyt

--- L11/Hw10.py
+++ L11/Hw10.py
@@ -3,8 +3,11 @@ def char_count(text:str,char:str)->int:
     if type(text)is not str or type(char) is not str:
         print("Error: Invalid type")
         return -1
-    if len(char)is not 1:
-        print("Error:Enter just one character")
+    if text == "" or char == "":
+        print("Error: Text or Char can't be empty")
+        return -1
+    if len(char) != 1:
+        print("Error: Enter just one character")
         return -1
     count=0
     digit=0
@@ -17,8 +20,33 @@ def char_count(text:str,char:str)->int:
     return count
 
 print(char_count("Hellloo","l"))
-# Task2
-def  print_str_with_space(text1:str)->str:
+
+# # Task2
+def  print_str_with_space(text1:str)->None:
     if type(text1) is not str:
         print("Error:Invalid type")
-        return "Error"
+        return
+    ser=0
+    while ser< len(text1):
+        print(text1[ser],end=" ")
+        ser+=1
+
+
+print_str_with_space("hello")
+print()
+
+# Task3
+def reverse_number(num: int) -> None:
+    if type(num) is not int:
+        print("Error: Invalid type")
+        return
+    if num<0:
+        num*=-1
+    num = str(num)
+    length1 = len(num) - 1
+    while length1 >= 0:
+        print(num[length1], end=" ")
+        length1 -= 1
+
+
+reverse_number(1234)
---
+++ L12/Cw12.py
@@ -0,0 +1,6 @@
+def reverse_str (text:str)-> str:
+   for index in range(len(text) - 1, -1, -1):
+        print(index)
+
+
+reverse_str("Hello")
