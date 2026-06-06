e1df7ac1dbe16fec6de0712d11e7478bc7c5779b
Jessy Gray <Jessy_Gray@icloud.com>
2026-06-05 20:35:30 -0700

    asd

--- L17/computer_c.py
+++ L17/computer_c.py
@@ -1,15 +1,63 @@
 class Computer:
+    min_ram = 1024          # 1 GB
+    max_ram = 1048576       # 1 TB
+
+    min_ssd = 128           # 128 GB
+    max_ssd = 10000000
+
     def __init__(self, brand: str, cpu: str, ram: int, ssd: int):
-        self.__brand = brand
-        self.__cpu = cpu
-        self.__ram = ram
-        self.__ssd = ssd
+        self.__brand = "Unknown"
+        self.__cpu = "Unknown"
+        self.__ram = 0
+        self.__ssd = 0
+
+        self.set_brand(brand)
+        self.set_cpu(cpu)
+        self.set_ram(ram)
+        self.set_ssd(ssd)
 
     def info(self):
-        print("Brand", self.brand)
-        print("CPU", self.cpu)
-        print("RAM", self.ram)
-        print("SSD", self.ssd)
+        print("Brand", self.__brand)
+        print("CPU", self.__cpu)
+        print("RAM", self.__ram)
+        print("SSD", self.__ssd)
 
     def __str__(self):
-        return f"My computer is a {self.brand} with {self.cpu} CPU, ~{self.ram} MB RAM, and ~{self.ssd} MB SSD."
+        return f"My computer is a {self.__brand} with {self.__cpu} CPU, ~{self.__ram} MB RAM, and ~{self.__ssd} MB SSD."
+
+    def __repr__(self):
+        return f"Computer('{self.__brand}', '{self.__cpu}', {self.__ram}, {self.__ssd})"
+
+    def get_brand(self):
+        return self.__brand
+
+    def set_brand(self, brand):
+        if isinstance(brand, str):
+            brand = brand.strip()
+            if len(brand) >= 2:
+                self.__brand = brand
+
+    def get_cpu(self):
+        return self.__cpu
+
+    def set_cpu(self, cpu):
+        if isinstance(cpu, str):
+            cpu = cpu.strip()
+            if len(cpu) >= 2:
+                self.__cpu = cpu
+
+    def get_ram(self):
+        return self.__ram
+
+    def set_ram(self, ram):
+        if isinstance(ram, int) and not isinstance(ram, bool):
+            if self.min_ram <= ram <= self.max_ram:
+                self.__ram = ram
+
+    def get_ssd(self):
+        return self.__ssd
+
+    def set_ssd(self, ssd):
+        if isinstance(ssd, int) and not isinstance(ssd, bool):
+            if self.min_ssd <= ssd <= self.max_ssd:
+                self.__ssd = ssd
--- L17/employee_c.py
+++ L17/employee_c.py
@@ -1,15 +1,23 @@
 class Employee:
-    def __init__(self, name: str, position: str, salary: float, department: str):
-        self.__name = name
-        self.__position = position
-        self.__salary = salary
-        self.__department = department
+    min_salary = 5000
+    max_salary = 100000
+
+    def __init__(self, name: str, position: str, salary: float | int, department: str):
+        self.__name = "Unknown"
+        self.__position = "Unknown"
+        self.__salary = 0.0
+        self.__department = "Unknown"
+
+        self.set_name(name)
+        self.set_position(position)
+        self.set_salary(salary)
+        self.set_department(department)
 
     def info(self):
-        print("Name", self.name)
-        print("Position", self.position)
-        print("Salary", self.salary)
-        print("Department", self.department)
+        print("Name", self.__name)
+        print("Position", self.__position)
+        print("Salary", self.__salary)
+        print("Department", self.__department)
 
     def __str__(self):
         return f"Employee {self.__name}, position: {self.__position}, salary: {self.__salary}, department: {self.__department}"
@@ -17,4 +25,37 @@ class Employee:
     def __repr__(self):
         return f"Employee('{self.__name}', '{self.__position}', {self.__salary}, '{self.__department}')"
 
-    def get.name():
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        if isinstance(name, str):
+            name = name.strip()
+            if len(name) >= 2:
+                self.__name = name
+
+    def get_position(self):
+        return self.__position
+
+    def set_position(self, position):
+        if isinstance(position, str):
+            position = position.strip()
+            if len(position) >= 2:
+                self.__position = position
+
+    def get_salary(self):
+        return self.__salary
+
+    def set_salary(self, salary):
+        if isinstance(salary, (int, float)) and not isinstance(salary, bool):
+            if self.min_salary <= salary <= self.max_salary:
+                self.__salary = salary
+
+    def get_department(self):
+        return self.__department
+
+    def set_department(self, department):
+        if isinstance(department, str):
+            department = department.strip()
+            if len(department) >= 2:
+                self.__department = department
